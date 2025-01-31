e996444273aaa3689ab187b5e96b8672af0b32d7
Jordi Santos Torres <jsantorupv@gmail.com>
2025-01-31 19:44:15 +0100

    IncioEsports
    He creado las vistas, los controladores y modelos de tipo recurso (1 d ellos no lo es)

--- fluxvidjordisantos/app/Models/Director.php
+++ fluxvidjordisantos/app/Models/Director.php
@@ -1,13 +1,10 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Director extends Model
 {
-    use HasFactory;
-
     protected $data = ['name', 'birthday', 'nacionality'];
 
     //Relación: un director tiene muchas películas.
--- fluxvidjordisantos/app/Models/movie.php
+++ fluxvidjordisantos/app/Models/movie.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    use HasFactory;
-
     protected $data = ['title', 'year', 'plot', 'rating', 'visibility', 'director_id'];
 
     //Relación: una película pertenece a un director.
