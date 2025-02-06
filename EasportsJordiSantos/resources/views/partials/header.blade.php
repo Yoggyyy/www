bc642e78fddd93d946de8d4b16d2c7902fc4ff85
Jordi Santos Torres <jsantorupv@gmail.com>
2025-02-06 20:34:53 +0100

    AvanzandoEasports

--- EasportsJordiSantos/resources/views/partials/header.blade.php
+++ EasportsJordiSantos/resources/views/partials/header.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+<div class="header-main">
+
+<div class="logo ">
+    <!-- User no registrado -->
+    <a href="{{route('register')}}">Registro</a>
+    <a href="{{route('login')}}">Login</a>
+    <!-- User registrado -->
+    <a href="{{route('logout')}}">Salir</a>
+    <a href="{{route('')}}">Cuenta</a>
+    <a href="{{route('index')}}">Inicio</a>
+    <a href="{{route('events')}}">Eventos</a>
+    <a href="{{route('players')}}">Jugadores</a>
+    <a href="{{route('contact')}}">Contacto</a>
+    <a href="{{route('where')}}">Donde Estamos</a>
+    <!-- User rol admin  -->
+    <a href="{{route('players.create')}}"></a>
+    <a href="{{route('events.create')}}"></a>
+    <a href="{{route('')}}"></a>
+    <a href="{{route('')}}"></a>
+</div>
+
 </div>
--- EasportsJordiSantos/routes/web.php
+++ EasportsJordiSantos/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\PlayerController;
+
+
+
+Route::resource('players', PlayerController::class)->except([
+    'update', 'store'
+]);
+
+Route::resource('events', EventController::class)->except([
+    'update', 'store'
+]);
+
+Route::resource('messages', MessageController::class)->except([
+    'update', 'store'
+]);
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
